90508500c59eb4f2d93dac6df09cb68f0dfda28c
Maurício Bruno <mb.bruno.s@gmail.com>
2021-06-23 12:00:55 -0300

    Update resources

--- public/src/formats-config.js
+++ public/src/formats-config.js
@@ -2,29 +2,22 @@ const formats = {
   "EJ-feed": {
     width: 1280, height: 1280,
     size: 45,
-    colors: ['#ffffff', '#f7d957'],
+    colors: ['#ffffff'],
   },
   "EJ-story": {
     width: 1080, height: 1920,
     size: 35,
-    colors: ['#ffffff', '#f7d957'],
+    colors: ['#ffffff'],
   },
   "EJ-wide": {
     width: 1920, height: 1080,
     size: 35,
-    colors: ['#ffffff', '#f7d957'],
+    colors: ['#ffffff'],
   },
   "EJ-panfleto": {
     width: 2480, height: 3508,
-    font: 'Second-Font',
     size: 100,
-    colors: ['#ffffff', '#f7d957'],
-  },
-  "algo-a-mais": {
-    width: 1080, height: 1080,
-    font: 'Algo-A-Mais-Font',
-    size: 35,
-    colors: ['#ffffff', '#000000'],
+    colors: ['#ffffff'],
   }
 };
 
